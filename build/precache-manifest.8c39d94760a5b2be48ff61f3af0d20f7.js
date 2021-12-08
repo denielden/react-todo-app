fceaeb745475bfb63a7c3a2858e1efc67d9b219b
Deniel Den <deniel.den.1998@gmail.com>
2021-12-08 14:50:50 +0100

    commit

---
+++ build/precache-manifest.8c39d94760a5b2be48ff61f3af0d20f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c604379d819d272d26452ed6ab93ff3f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f8eb7f429ded76175ee8",
+    "url": "/react-todo-app/static/css/main.1a8bc710.chunk.css"
+  },
+  {
+    "revision": "e0f657c6c9cc357b412b",
+    "url": "/react-todo-app/static/js/2.56600beb.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-todo-app/static/js/2.56600beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911e48db4a2082cbc5a6",
+    "url": "/react-todo-app/static/js/3.b08559a1.chunk.js"
+  },
+  {
+    "revision": "f8eb7f429ded76175ee8",
+    "url": "/react-todo-app/static/js/main.4d83ab9c.chunk.js"
+  },
+  {
+    "revision": "31e2d069ec2086a77994",
+    "url": "/react-todo-app/static/js/runtime-main.403ea534.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-todo-app/static/media/logo.06e73328.svg"
+  }
+]);
